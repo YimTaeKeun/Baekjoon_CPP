7307a2cb875eaef816b1e77025887d7ca07bec99
YimTaekeun <ytk030305@gmail.com>
2024-01-05 12:07:27 +0900

    Success

---
+++ 15680.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 0; 
+    cin >> n;
+    if(n == 0) cout << "YONSEI";
+    else cout << "Leading the Way to the Future";
+    cout << endl;
+    return 0;
+}
