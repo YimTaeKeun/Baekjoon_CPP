459405102cbb0052d04fd324248035dc470a9c17
YimTaekeun <ytk030305@gmail.com>
2024-01-22 12:24:39 +0900

    Success

---
+++ 6810.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int one = 0, two = 0, three = 0;
+    cin >> one >> two >> three;
+    cout << "The 1-3-sum is " << 91 + one + (two * 3) + three << endl;
+    return 0;
+}
