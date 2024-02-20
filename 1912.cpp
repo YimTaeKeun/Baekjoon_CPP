39fc5e31f6bd6712c8354eaf8d808d4ce98f857b
YimTaekeun <ytk030305@gmail.com>
2024-02-20 23:48:33 +0900

    Success

--- 1912.cpp
+++ 1912.cpp
@@ -1,28 +1,22 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int* board;
-    long long* sumBoard;
+    long long* board;
+    long long* dp;
     int cnt = 0;
     cin >> cnt;
-    board = new int[cnt];
-    sumBoard = new long long[cnt];
+    board = new long long[cnt];
+    dp = new long long[cnt];
     for(int i = 0; i < cnt; i++) cin >> board[i];
-    long long sum = 0;
-    for(int i = 0; i < cnt; i++){
-        sum += board[i];
-        sumBoard[i] = sum;
-    }
-    long long maxNum = sumBoard[0];
-    for(int i = 0; i < cnt; i++){
-        long long target = sumBoard[i];
-        if(target > maxNum) maxNum = target;
-        for(int j = 0; j < i; j++){
-            if(target - sumBoard[j] > maxNum) maxNum = target - sumBoard[j];
-        }
+    dp[0] = board[0];
+    long long maxNum = board[0];
+    for(int i = 1; i < cnt; i++){
+        dp[i] = max(dp[i - 1] + board[i], board[i]);
+        if(dp[i] > maxNum) maxNum = dp[i];
     }
     cout << maxNum;
     return 0;
