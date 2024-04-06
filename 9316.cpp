c12cfe439f4891abd3475e37782261a214d2b4e3
YimTaekeun <ytk030305@gmail.com>
2024-04-06 10:19:55 +0900

    Success

---
+++ 9316.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 0;
+    cin >> n;
+    for(int i = 1; i <= n; i++) cout << "Hello World, Judge " << i << "!" << endl;
+    return 0;
+}
